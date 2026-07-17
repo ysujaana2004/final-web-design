dca0609b4674baab817dbc9d483b7197c7e66017
sahafkhan <sahafkhan2@gmail.com>
2026-07-17 18:32:15 -0400

    Add dashboard route

--- frontend/src/App/App.jsx
+++ frontend/src/App/App.jsx
@@ -5,7 +5,7 @@ import Recipes from "../Recipes/Recipies.jsx";
 import Pantry from "../Pantry/Pantry.jsx";
 import Login from "../Login/Login.jsx";
 import GroceryList from "../Grocery List/GroceryList.jsx";
-
+import DashboardChart from "../Dashboard/DashboardChart.jsx";
 
 
 function App() {
@@ -18,6 +18,7 @@ function App() {
         <Route path="/pantry" element={<Pantry />} />
         <Route path="/login" element={<Login />} />
         <Route path="/grocery" element={<GroceryList />} />
+        <Route path="/dashboard" element={<DashboardChart />} />
 
         <Route
           path="*"
